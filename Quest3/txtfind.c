c091eb7333601503f8e89af7ecf08d0a37e676a4
avi barel <maccavi2@gmail.com>
2019-12-18 01:10:45 +0200

    4th uplaod

--- Quest3/txtfind.c
+++ Quest3/txtfind.c
@@ -9,7 +9,7 @@
 int Get_Line(char s[LINE]){
     char temp;
     int size=0;
-    for(int i=0;i<LINE;i++){
+    for(size_t i=0;i<LINE;i++){
       printf("apply a letter \n");
       scanf("%c",&temp);
       s[i]=temp;
@@ -51,7 +51,7 @@ int similar(char *s,char *t,int n){
 int tindex=0,start,wordlen,count=0;
 bool isStart=false;
 wordlen=strlen(t)+n;
-for(int i=0;i<strlen(s);i++){
+for(size_t i=0;i<strlen(s);i++){
   if(s[i]!=t[tindex]){
     count++;
   }
@@ -74,12 +74,59 @@ isStart=false;
 return 0;
 }
 void print_line(char* str){
-  char* line;
+  bool isStart=false,isEnd=false;
+  char line[strlen(str)],pr;
+  int linestart=0,linend=0,print=0;
 printf("add your text now \n");
-scanf("%c",line);
+scanf("%c",&pr);
  for (size_t i = 0; i < strlen(str); i++)
  {
- 
+     if(!isStart){
+       linestart=i;
+       isStart=true;
+     }
+     if(str[i]=='\n'){
+       linend=i;
+       isEnd=true;
+     }
+     if(!isEnd){
+       line[i]=str[i];
+     }
+     if(isEnd){
+      print=substring(*line,*pr);
+      if(print==1){
+        printf("%s \n",line);
+      }
+      linestart=i+1;
+      isEnd=false;
+      isStart=false;
+     }
+   }
  }
- 
-}
+ void print_similar_words(char *str){
+      bool isStart=false,isEnd=false;
+      char line[strlen(str)+1],pr;
+      int lstart=0,j=0,tindex=0;
+       printf("add your text now \n");
+       scanf("%c",&pr);
+ for (size_t i = 0; i < strlen(str); i++)
+   {
+     if(str[i]==pr[tindex]){
+       lstart=i+1;
+       while(j<strlen(line) && i>(strlen(str)-strlen(pr))){
+         line[j]=str[i+j];
+         j++;
+       }
+       while(j<strlen(pr) && i==(strlen(str)-strlen(pr))){
+           line[j]=str[i+j];
+           j++;
+       }
+       j=0;
+       if(similar(*line,*pr,1)==1){
+          printf("%c \n",line);
+       }
+       else 
+          i=lstart;
+       }
+     }
+   }
