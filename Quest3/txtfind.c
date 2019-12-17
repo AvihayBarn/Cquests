7c93d26d1eed92afb9c309bae390ac1167192e60
avi barel <maccavi2@gmail.com>
2019-12-17 21:30:15 +0200

    2nd uplaod

--- Quest3/txtfind.c
+++ Quest3/txtfind.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 #define LINE 256
 
-void getline(char[] s){
-    for(int i=0;i<s.length;i++){
-    char temp=*(s+i)
+int Get_Line(char s[LINE]){
+    char temp;
+    int size=0;
+    for(int i=0;i<LINE;i++){
+      printf("apply a letter \n");
+      scanf("%c",&temp);
+      s[i]=temp;
+      if(s[i-1]=='/' && s[i]=='n'){
+        size=i+1;
+      break;
+      }
     }
-}
+    return size;
+    }
+
 
 int substring(char *str1,char *str2){
-  *p=strstr(str1,str2);
+    char *p=strstr(str1,str2);
     if(p)
     return 1;
     return 0;
@@ -18,8 +31,8 @@ int substring(char *str1,char *str2){
 int similar(char *s,char *t,int n){
 int tindex=0,start,end,wordlen,count=0;
 bool isStart=false;
-wordlen=strlen(t)+n;
-for(int i=0;i<strlen(s);i++){
+wordlen=sizeof(t)+n;
+for(int i=0;i<sizeof(s);i++){
   if(s[i]!=t[tindex]){
     count++;
   }
@@ -29,7 +42,7 @@ if(s[i]==t[tindex] && !isStart){
    tindex++;
 }
 if(count==n){
- if(i-start+n)==wordlen){
+ if((i-start+n)==wordlen){
  return 1;
 }
 else 
@@ -40,4 +53,7 @@ isStart=false;
 }
 }
 return 0;
+}
+void print_line(char* str){
+
 }
