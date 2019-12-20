1d29b8d0bb29fc40951a521136a3dea7a802bfa2
AviBoter <54690196+AviBoter@users.noreply.github.com>
2019-12-20 16:49:32 +0200

    Add files via upload

---
+++ Quest3/txtfind.h
@@ -0,0 +1,11 @@
+#ifndef txtfind
+#define txtfind
+
+  int Get_Line(char s[]);
+  int getword(char w[]);
+  int substring(char *str1,char *str2);
+  int similar(char *s, char *t, int n);
+  void print_line(char* str);
+  void print_similar_words(char *str);
+
+#endif
