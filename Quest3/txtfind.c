921631edd1d5f1fc40c57d9f5de0b1152aad3a03
AviBoter <54690196+AviBoter@users.noreply.github.com>
2019-12-21 15:39:39 +0200

    Add files via upload

--- Quest3/txtfind.c
+++ Quest3/txtfind.c
@@ -26,13 +26,14 @@ char temp;
     int size=0;
     for(int i=0;i<WORD;i++){
       scanf("%c",&temp);
+      if(temp!='\n' || temp!='\t' || temp!='\0')
       w[i]=temp;
       if(temp=='\n' || temp=='\t' || temp=='\0'){
         size=i;
       i=WORD-1;
-      w[i]='\0';
       }
     }
+    w[size]='\0';
     return size;
 }
 
@@ -48,7 +49,8 @@ int substring(char *str1,char *str2){
 
 int similar(char *s, char *t, int n)
 {
-  int Ssize = strlen(s), Tsize = strlen(t);
+  int Ssize = StrLength(s);
+  int Tsize = StrLength(t);
   if ((Ssize - n) != Tsize)
   {
     return 0;
@@ -67,15 +69,12 @@ int similar(char *s, char *t, int n)
     }
     if (count == n && (i + 1) == Ssize && tindex == Tsize)
     {
-      printf(" %d \t%d \t %d \n",i,tindex,count);
       return 1;
     }
     if (count > n)
     {
-      printf(" %d \n",i);
       return 0;
     }
-    printf(" %d \t%d \t %d \n",i,tindex,count);
   }
   return 0;
 }
@@ -89,24 +88,31 @@ void print_line(char* str){
         break;
       }
       if(substring(line,str)==1){
-        printf("%s\n",line);
+        printf("%s \n",line);
         }
         line[0]='\0';
       }
    }
 
  void print_similar_words(char *str){
-       int print=0,j=0;
-  char newC[WORD];
+       int print=0;
+       char newC[WORD];
  for (size_t i = 0; i <LINE*LINE; i++){
       print=getword(newC);
       if(print==0){
         break;
       }
-      if(similar(str,newC,1)==1){
+      if(similar(newC,str,1)==1){
         printf("%s \n",newC);
         }
         newC[0]='\0';
       }
    }
 
+   int StrLength(char *str){
+     int i=0;
+     while(str[i]!='\0'){
+       i++;
+     }
+     return i;
+   }
